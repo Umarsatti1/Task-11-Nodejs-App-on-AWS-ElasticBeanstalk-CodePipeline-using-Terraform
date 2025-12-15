e170d8026e29cd1bd3110ad784f6b61b23c4c9fb
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-12-16 04:08:24 +0500

    Update connection.js

--- E-Commerce-react-node-mysql/server/database/connection.js
+++ E-Commerce-react-node-mysql/server/database/connection.js
@@ -5,14 +5,14 @@ require('dotenv').config(); // Load environment variables from .env file
 let connectionParams;
 
 // Use flag to toggle between localhost and server configurations
-const useLocalhost = process.env.USE_LOCALHOST === 'true';
+const useLocalhost = process.env.USE_LOCALHOST === 'false';
 
 if (useLocalhost) {
     console.log("Inside local")
     connectionParams = {
         user: "root",
         host: "localhost",
-        password: "Sirsatti123!",
+        password: "P@ssw0rd",
         database: "e_commerce",
     };
 } else {
