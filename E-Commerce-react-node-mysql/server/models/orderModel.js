3eaf93e8b95d7f5cc0ef4c5b41ee812158cbf8be
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-12-16 21:23:45 +0500

    Update models

--- E-Commerce-react-node-mysql/server/models/orderModel.js
+++ E-Commerce-react-node-mysql/server/models/orderModel.js
@@ -1,92 +1,73 @@
 // orderModel.js
-
 const pool = require("../database/connection");
-exports.getAllOrders = () => {
-    return new Promise((resolve, reject) => {
-        pool.query(
+
+exports.getAllOrders = async () => {
+    try {
+        const [rows] = await pool.query(
             "SELECT O.orderId, U.fname, U.lname, O.createdDate, O.totalPrice " +
-            "FROM orders O INNER JOIN users U ON O.userId = U.userId;",
-            (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            }
+            "FROM orders O INNER JOIN users U ON O.userId = U.userId;"
         );
-    });
+        return rows;
+    } catch (err) {
+        console.error("getAllOrders error:", err.message);
+        throw err;
+    }
 };
 
-
-exports.getOrderById = (orderId) => {
-    return new Promise((resolve, reject) => {
-        pool.query(
+exports.getOrderById = async (orderId) => {
+    try {
+        const [rows] = await pool.query(
             "SELECT U.fname, U.lname, O.totalPrice, U.createdDate, O.address " +
             "FROM orders O INNER JOIN users U ON O.userId = U.userId " +
             "WHERE O.orderId = ?;",
-            [orderId],
-            (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            }
+            [orderId]
         );
-    });
+        return rows;
+    } catch (err) {
+        console.error("getOrderById error:", err.message);
+        throw err;
+    }
 };
 
-exports.getProductsByOrder = (orderId) => {
-    return new Promise((resolve, reject) => {
-        pool.query(
+exports.getProductsByOrder = async (orderId) => {
+    try {
+        const [rows] = await pool.query(
             "SELECT P2.productId, P2.name, P.quantity, P.totalPrice " +
             "FROM orders O INNER JOIN productsInOrder P ON O.orderId = P.orderId " +
             "INNER JOIN product P2 ON P.productId = P2.productId " +
             "WHERE O.orderId = ?;",
-            [orderId],
-            (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            }
+            [orderId]
         );
-    });
+        return rows;
+    } catch (err) {
+        console.error("getProductsByOrder error:", err.message);
+        throw err;
+    }
 };
 
+exports.updateOrder = async (orderId, newData) => {
+    try {
+        const [result] = await pool.query("UPDATE orders SET ? WHERE orderId = ?", [newData, orderId]);
+        return result;
+    } catch (err) {
+        console.error("updateOrder error:", err.message);
+        throw err;
+    }
+};
 
-exports.updateOrder = (orderId, newData) => {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            "UPDATE orders SET ? WHERE orderId = ?",
-            [newData, orderId],
-            (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            }
+exports.getPastOrdersByCustomerID = async (customerId) => {
+    try {
+        const [rows] = await pool.query(
+            "SELECT O.orderId, P.name, O.createdDate, PIN.quantity, PIN.totalPrice " +
+            "FROM orders O INNER JOIN productsInOrder PIN ON O.orderId = PIN.orderId  " +
+            "INNER JOIN product P ON PIN.productId = P.productId " +
+            "WHERE O.userId = ? " +
+            "ORDER BY O.orderID DESC;",
+            [customerId]
         );
-    });
+        return rows;
+    } catch (err) {
+        console.error("getPastOrdersByCustomerID error:", err.message);
+        throw err;
+    }
 };
-
-exports.getPastOrdersByCustomerID = (orderId) => {
-    const query =
-        "SELECT O.orderId, P.name, O.createdDate, PIN.quantity, PIN.totalPrice " +
-        "FROM orders O INNER JOIN productsInOrder PIN ON O.orderId = PIN.orderId  " +
-        "INNER JOIN product P ON PIN.productId = P.productId " +
-        "WHERE O.userId = ? " +
-        "ORDER BY O.orderID DESC;";
-    return new Promise((resolve, reject) => {
-        pool.query(query, [orderId], (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
-    });
-};
-
--- E-Commerce-react-node-mysql/server/models/productModel.js
+++ E-Commerce-react-node-mysql/server/models/productModel.js
@@ -1,94 +1,75 @@
 // productModel.js
-
 const pool = require("../database/connection");
 
-exports.getAllProducts = () => {
-    return new Promise((resolve, reject) => {
-        pool.query("SELECT * FROM product;", (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
-    });
+exports.getAllProducts = async () => {
+    try {
+        const [rows] = await pool.query("SELECT * FROM product;");
+        return rows;
+    } catch (err) {
+        console.error("getAllProducts error:", err.message);
+        throw err;
+    }
 };
 
-
-exports.getProductDetailsById = (productId) => {
-    return new Promise((resolve, reject) => {
-        const query =
-            "SELECT * FROM product WHERE productId = ?";
-        pool.query(query, [productId], (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
-    });
+exports.getProductDetailsById = async (productId) => {
+    try {
+        const [rows] = await pool.query("SELECT * FROM product WHERE productId = ?", [productId]);
+        return rows;
+    } catch (err) {
+        console.error("getProductDetailsById error:", err.message);
+        throw err;
+    }
 };
 
-exports.allOrderByProductId = (productId) => {
-    return new Promise((resolve, reject) => {
-        const query =
+exports.allOrderByProductId = async (productId) => {
+    try {
+        const [rows] = await pool.query(
             "SELECT O.orderId, U.fname, U.lname, O.createdDate, PIN.quantity, PIN.totalPrice " +
             "FROM users U INNER JOIN orders O on U.userId  = O.userId " +
             "INNER JOIN productsInOrder PIN on O.orderId = PIN.orderId " +
             "INNER JOIN product P on PIN.productId = P.productId " +
-            "WHERE PIN.productId = ?;";
-
-        pool.query(query, [productId], (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
-    });
-};
-
-
-exports.createProduct = (name, price, description) => {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            "INSERT INTO product (name, price, description) VALUES (?,?,?);",
-            [name, price, description],
-            (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            }
+            "WHERE PIN.productId = ?;",
+            [productId]
         );
-    });
+        return rows;
+    } catch (err) {
+        console.error("allOrderByProductId error:", err.message);
+        throw err;
+    }
 };
 
-exports.updateProduct = (productId, name, price, description) => {
-    return new Promise((resolve, reject) => {
-        pool.query(
+exports.createProduct = async (name, price, description) => {
+    try {
+        const [result] = await pool.query(
+            "INSERT INTO product (name, price, description) VALUES (?,?,?)",
+            [name, price, description]
+        );
+        return result;
+    } catch (err) {
+        console.error("createProduct error:", err.message);
+        throw err;
+    }
+};
+
+exports.updateProduct = async (productId, name, price, description) => {
+    try {
+        const [result] = await pool.query(
             "UPDATE product SET name = ?, price = ?, description = ? WHERE productId = ?",
-            [name, price, description, productId],
-            (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            }
+            [name, price, description, productId]
         );
-    });
+        return result;
+    } catch (err) {
+        console.error("updateProduct error:", err.message);
+        throw err;
+    }
 };
 
-exports.deleteProduct = (productId) => {
-    return new Promise((resolve, reject) => {
-        pool.query("DELETE FROM product WHERE productId = ?", [productId], (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
-    });
+exports.deleteProduct = async (productId) => {
+    try {
+        const [result] = await pool.query("DELETE FROM product WHERE productId = ?", [productId]);
+        return result;
+    } catch (err) {
+        console.error("deleteProduct error:", err.message);
+        throw err;
+    }
 };
