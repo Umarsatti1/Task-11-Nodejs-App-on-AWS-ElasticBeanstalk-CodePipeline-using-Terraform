8b60df10b94da7e0cb1611e7f0a6d06a07caf561
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-12-16 21:49:48 +0500

    Update app.js

--- E-Commerce-react-node-mysql/server/app.js
+++ E-Commerce-react-node-mysql/server/app.js
@@ -24,7 +24,7 @@ app.use("/api/token", userToken);
 // Serve React build
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
