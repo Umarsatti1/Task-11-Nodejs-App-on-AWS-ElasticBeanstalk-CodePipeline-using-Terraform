ba1931b78b356f86db93f9ebafccaee20ab67036
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-12-16 15:02:21 +0500

    Update apiConfig.js

--- E-Commerce-react-node-mysql/client/src/components/apiConfig.js
+++ E-Commerce-react-node-mysql/client/src/components/apiConfig.js
@@ -1,5 +1,6 @@
 
 // apiConfig.js
 export const getBaseURL = () => {
-    return process.env.REACT_APP_API_URL || "http://localhost:3001/";
+    return "/";
+    //process.env.REACT_APP_API_URL || "http://localhost:3001/";
 };
